8a7ad88000fdc29c5734169b268cf1d45f2a6fdd
sbielaws <szymonbielawski@gmail.com>
2018-05-15 16:15:59 +0200

    Initial commit

---
+++ src/EarthquakeCityMap.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.*;
+
+import processing.core.PApplet;
+
+import parsing.ParseFeed;
+
+public class EarthquakeCityMap extends PApplet {
+}
---
+++ src/SimpleLocation.java
@@ -0,0 +1,9 @@
+public class SimpleLocation {
+    public double lat;
+    public double lon;
+
+    public SimpleLocation (double latIn, double lonIn){
+        this.lat = latIn;
+        this.lon = lonIn;
+    }
+}
